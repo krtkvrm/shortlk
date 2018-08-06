c0ac43eb3aa4e78890f6e38dcb60b2dee51502b9
Kartik Verma <3920286+vkartik97@users.noreply.github.com>
2018-08-06 21:00:27 +0530

    Added Class DB

---
+++ db.py
@@ -0,0 +1,48 @@
+from __future__ import print_function
+import pymysql as mysql
+import os
+
+class DB:
+    def __init__(self, debug=False):
+        self.db = mysql.connect(os.environ['db_host'], 
+                                os.environ['db_user'], 
+                                os.environ['db_passwd'], 
+                                os.environ['db_prod'])   
+        self.cursor = self.db.cursor()
+        self.debug = debug
+
+    def search(self, tables, columns="*", where='TRUE'):
+        query = """
+                SELECT {0} 
+                FROM {1} 
+                WHERE {2};
+                """.format(columns, tables, where)
+        
+        if self.debug:
+            print(query)
+
+        self.cursor.execute(query)
+        return self.cursor.fetchall()
+
+    def add(self, table, values, columns=''):
+        if columns :
+            columns = '({})'.format(columns)
+        
+        query = """
+                INSERT INTO {0}{1} 
+                VALUES ({2}) ;
+                """.format(table, columns, values)
+
+        if self.debug :
+            print(query)
+
+        try:
+            self.cursor.execute(query)
+            self.db.commit()
+            return 0
+        except :
+            self.db.rollback()
+            return 1
+
+    def __del__(self):
+        self.db.close()
