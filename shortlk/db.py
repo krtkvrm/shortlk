992394f828bb701d9ccdd07cc7007d8c6d26e73c
Kartik Verma <3920286+vkartik97@users.noreply.github.com>
2018-08-10 11:19:33 +0530

    Handling Web Interface

--- shortlk/db.py
+++ shortlk/db.py
@@ -2,22 +2,26 @@ from __future__ import print_function
 import pymysql as mysql
 import os
 
+
 class DB:
     def __init__(self, debug=False):
-        self.db = mysql.connect(os.environ['db_host'], 
-                                os.environ['db_user'], 
-                                os.environ['db_passwd'], 
-                                os.environ['db_prod'])   
+        self.db = mysql.connect(os.environ['db_host'],
+                                os.environ['db_user'],
+                                os.environ['db_passwd'],
+                                os.environ['db_prod'])
         self.cursor = self.db.cursor()
         self.debug = debug
 
-    def search(self, tables, columns="*", where='TRUE'):
+    def search(self, tables, columns="*", where='TRUE', index=''):
+        if index:
+            index = ""
+
         query = """
-                SELECT {0} 
-                FROM {1} 
+                SELECT {0}
+                FROM {1}
                 WHERE {2};
                 """.format(columns, tables, where)
-        
+
         if self.debug:
             print(query)
 
@@ -25,21 +29,21 @@ class DB:
         return self.cursor.fetchall()
 
     def add(self, table, values, columns=''):
-        if columns :
+        if columns:
             columns = '({})'.format(columns)
-        
+
         query = """
-                INSERT INTO {0}{1} 
+                INSERT INTO {0}{1}
                 VALUES ({2}) ;
                 """.format(table, columns, values)
 
-        if self.debug :
+        if self.debug:
             print(query)
 
         try:
             self.cursor.execute(query)
             self.db.commit()
             return 0
-        except :
+        except BaseException:
             self.db.rollback()
             return 1
---
+++ shortlk/server.py
@@ -0,0 +1,72 @@
+from flask import (Flask, request, redirect, render_template)
+import json
+from db import DB
+import random
+import string
+import re
+
+app = Flask(__name__)
+db = DB()
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def newURL():
+
+    print(request.data)
+
+    data = JSONStringer(request)
+
+    code = ''.join(random.choices(string.ascii_uppercase
+                                  + string.digits, k=random.randint(2, 5)))
+    print(data)
+    result = db.search(
+        'urls',
+        'short_code',
+        where="url = '{0}'".format(
+            data['url']))
+
+    if not result:
+        while db.add('urls', " '{}', '{}' ".format(code, data['url'])):
+            code = ''.join(
+                random.choices(
+                    string.ascii_uppercase +
+                    string.digits,
+                    k=random.randint(
+                        2,
+                        5)))
+    else:
+        code = result[0][0]
+
+    res = {'code': code}
+    return json.dumps(res)
+
+
+@app.route('/<url>', methods=['GET'])
+def redirect_url(url=''):
+    result = db.search('urls', 'url', where="short_code = '{}'".format(url))
+
+    if result:
+        if not re.search(r"(?:f|ht)(?:tp|tps):\/\/", result[0][0]):
+            return redirect("http://" + result[0][0], code=301)
+        else:
+            return redirect(result[0][0], code=301)
+    else:
+        return "404 Error", 404
+
+
+def JSONStringer(req):
+    try:
+        data = json.loads(req.data, strict=False)
+        return data
+    except json.decoder.JSONDecodeError:
+        return "Decoding Error"
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(port=8001, host='0.0.0.0')
