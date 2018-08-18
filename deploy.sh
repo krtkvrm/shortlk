d2be9e540174b4fc6f2920192cba46a5eedca9a0
Kartik Verma <3920286+vkartik97@users.noreply.github.com>
2018-08-18 18:01:01 +0530

    Added API Call to Clear Cache

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+curl -s -X POST $(echo $cache_url) \
+    -H "X-Auth-Email: $cache_email " \
+    -H "X-Auth-Key: $cache_key" \
+    -H "Content-Type: application/json" \
+    --data '{"purge_everything":true}' > /dev/null
+
+echo "Cache Cleared"
+
 echo "Deployment Started!"
 
 rsync -r -I --quiet $TRAVIS_BUILD_DIR ubuntu@ec2-52-206-3-72.compute-1.amazonaws.com:apps
