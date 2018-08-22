e2b598dbda6b2d07a1fcc30131cd2734724e43b1
Kartik Verma <3920286+vkartik97@users.noreply.github.com>
2018-08-23 02:21:33 +0530

    Automatic Deployment to Netlify and To Fix script.js :heavy_exclamation_mark:

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,23 @@ curl -s -X POST $(echo $cache_url) \
 
 echo "Cache Cleared"
 
-echo "Deployment Started!"
+echo "Frond End Deployment"
+
+mkdir -p frontend/static
+
+sed -i -e 's/shortlk/frontend/g' .bowerrc
+
+bower install
+
+sed -i -e 's/frontend/shortlk/g' .bowerrc
+
+python3 shortlk/render.py > frontend/index.html
+
+cp -R  shortlk/static frontend
+
+./netlifyctl deploy -A $NETLIFY_KEY -s $NETLIFY_PROJECT_KEY -P frontend
+
+echo "API Deployment Started!"
 
 rsync -r -I --quiet $TRAVIS_BUILD_DIR ubuntu@ec2-52-206-3-72.compute-1.amazonaws.com:apps
 
--- shortlk/static/script.js
+++ shortlk/static/script.js
@@ -22,18 +22,18 @@ var concatHTTP = (url) => {
 var YourOnSubmitFn = ((token) => {
     var url = $('#url').val()
     jQuery.ajax ({
-        url: '/',
+        url: 'https://shortlk.gq/',
         type: "POST",
         data: JSON.stringify({url: url, token: token}),
         dataType: "json",
         contentType: "application/json; charset=utf-8",
         success: function(data){
-            var shortURL = window.location.host + "/" + data.code
+            var shortURL = "shortlk.gq/" + data.code
             var oldLink = ['<a href="'+ concatHTTP(url) +'" target="_blank"><h3 style="margin-bottom: 20px;">'+url+'</h3></a>',
                             '<div class="arrow_box  style="margin-bottom: 20px;"">',
                                 '<h3 class="logo">Converted to </h3>',
                             '</div>'].join('\n')
-            var newLink = ['<a href="' + window.location.protocol + '//' + shortURL +'" target="_blank"><h3 style="margin-top: 40px;">'+shortURL+'</h3></a>',
+            var newLink = ['<a href="' + "http://" + shortURL + '" target="_blank"><h3 style="margin-top: 40px;">'+shortURL+'</h3></a>',
                             '<img src="https://chart.googleapis.com/chart?cht=qr&chl='+shortURL+'L&chs=160x160&chld=L|0" style="margin-top: 40px;">',
                         ].join('\n')
             $('#main-block').html(oldLink+newLink)
