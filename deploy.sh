70f48355f04e707377f71b8f61f9cbf8063e7d39
Kartik Verma <3920286+vkartik97@users.noreply.github.com>
2018-09-07 14:23:02 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ echo "Cache Cleared"
 
 echo "Deployment Started!"
 
-rsync -r -I --quiet $TRAVIS_BUILD_DIR ubuntu@ec2-52-206-3-72.compute-1.amazonaws.com:apps
+rsync -r -I --quiet $TRAVIS_BUILD_DIR ubuntu@ec2-54-85-21-196.compute-1.amazonaws.com:apps
 
-ssh ubuntu@ec2-52-206-3-72.compute-1.amazonaws.com "cd apps/shortlk; sudo sh docker.sh"
+ssh ubuntu@ec2-54-85-21-196.compute-1.amazonaws.com "cd apps/shortlk; sudo sh docker.sh"
